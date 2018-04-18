eed33ce294d864fa7ac5b9e67c4ade746908e4a4
Josh <sodoffusillygit@gmail.com>
2018-04-18 11:49:21 -0500

    first commit

---
+++ Exceptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class TreeHouseDefenseException : System.Exception
+    {
+        public TreeHouseDefenseException()
+        {
+
+        }
+
+        public TreeHouseDefenseException(string message) : base(message)
+        {
+
+        }
+    }
+
+    class OutOfBoundsException : TreeHouseDefenseException
+    {
+        public OutOfBoundsException()
+        {
+
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+
+        }
+    }
+
+}
---
+++ Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+
+            Map map = new Map(8, 5);
+
+            try
+            {
+                MapLocation maplocation = new MapLocation(20, 20, map);
+            }
+            catch(OutOfBoundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(TreeHouseDefenseException)
+            {
+                Console.WriteLine("Unhandled TreeHouseDefenseException");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unhandled Exception");
+            }
+        }
+    }
+}
